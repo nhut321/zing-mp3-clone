7aa319428d0949f65b090e3e2b7dad923ca05e61
nhut321 <84066824+nhut321@users.noreply.github.com>
2026-02-17 04:35:49 +0700

    Update index.js

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,15 +1,6 @@
 import Board from '../Board'
 
 const Game = () => {
-    return (
-        /*
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-      </div>
-      */
-    );
 };
 
 export default Game
