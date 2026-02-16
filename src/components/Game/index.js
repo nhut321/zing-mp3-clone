ef50316d2e73914807ff1020989582e1347b7699
nhut321 <84066824+nhut321@users.noreply.github.com>
2026-02-17 04:32:22 +0700

    Update index.js

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,11 +2,13 @@ import Board from '../Board'
 
 const Game = () => {
     return (
+        /*
       <div className="game">
         <div className="game-board">
           <Board />
         </div>
       </div>
+      */
     );
 };
 
